21236b3b3d87d9413b52c6bd77a09ce50047aced
GuilleObe8 <guilleobe8@gmail.com>
2024-02-23 16:14:41 +0100

    Friends working

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { StyleSheet, View, StatusBar } from "react-native";
-import MainStack from "./components/stacks/MainStack";
 import LoginStack from "./components/stacks/LoginStack";
-import Friends from "./components/screens/Friends";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor="#2B2B2B" barStyle="default" />
-      <Friends />
+      <LoginStack />
     </View>
   );
 }
