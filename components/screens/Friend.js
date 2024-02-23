4c744b8f5af76e68db79c1c562febfcb8bb0f40b
GuilleObe8 <guilleobe8@gmail.com>
2024-02-23 11:27:16 +0100

    Friends

---
+++ components/screens/Friend.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Friend = ({
+  userid,
+  firstName,
+  lastName,
+  tag,
+  pendingRequest,
+  activityData,
+}) => {
+  const responseTime = Math.round(
+    activityData.totalResponseTime / activityData.numberOfInvites
+  );
+
+  return (
+    <View>
+      <Text>{`${firstName} ${lastName}`}</Text>
+      <Text>{tag}</Text>
+      <Text>{`Response Time: ${responseTime} seconds`}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#2B2B2B",
+    alignItems: "center",
+  },
+});
+
+export default Friend;
