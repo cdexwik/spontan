025ea75bc7144ee7d3bb317eab25921ebdfa52bf
GuilleObe8 <guilleobe8@gmail.com>
2024-02-27 14:55:45 +0100

    Friends + Main + Activities

---
+++ src/components/AnsweredFriend.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import ProfilePictureFriend from "./ProfilePictureFriend";
+
+const AnsweredFriend = ({ firstName, lastName, tag, activityData }) => {
+  const responseTime = Math.round(
+    activityData.totalResponseTime / activityData.numberOfInvites
+  );
+
+  const acceptButtonHandler = () => {
+    console.log("Accept Pressed");
+  };
+
+  const crossButtonHandler = () => {
+    console.log("Cross Pressed");
+  };
+
+  return (
+    <View style={styles.friend}>
+      <ProfilePictureFriend />
+      <View style={styles.friendTextBox}>
+        <Text style={styles.friendTextName}>{`${firstName} ${lastName}`}</Text>
+        <Text style={styles.friendTextTag}>@{tag}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  friend: {
+    flexDirection: "row",
+  },
+  friendTextBox: {
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    paddingVertical: 10,
+    marginLeft: 20,
+  },
+  friendTextName: {
+    fontSize: 12,
+    color: "#F8F8F8",
+    fontWeight: "bold",
+    fontFamily: "Helvetica Neue",
+  },
+  friendTextTag: {
+    fontSize: 12,
+    fontStyle: "italic",
+    color: "#A0A0A0",
+    fontFamily: "Helvetica Neue",
+  },
+  friendText: {
+    fontSize: 12,
+    color: "#A0A0A0",
+    fontFamily: "Helvetica Neue",
+  },
+});
+
+export default AnsweredFriend;
